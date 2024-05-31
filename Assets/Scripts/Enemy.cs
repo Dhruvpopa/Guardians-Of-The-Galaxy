36cfde2d65aeafd4a9316b22e903478e6eb8b0e8
Dhruvpopa <202151050@iiitvadodara.ac.in>
2024-06-01 03:28:05 +0530

    Popa Enemy added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -39,6 +39,7 @@ public class Enemy : MonoBehaviour
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
+        scoreBoard.IncreaseScore(scorePerHit);
     }
 
     void ProcessHit()
@@ -46,6 +47,6 @@ public class Enemy : MonoBehaviour
         GameObject vfx1= Instantiate(deathVFX,transform.position, Quaternion.identity);
         vfx1.transform.parent = parentGameObject.transform;
         hitPoints--;
-        scoreBoard.IncreaseScore(scorePerHit);
+        
     }
 }
